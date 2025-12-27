69e0751feb4823d19524cfb3b24dc6d62b65d444
Adesola Oni <adesolaoni2001@gmail.com>
2025-12-27 07:00:08 +0100

    Delete dist directory

--- dist/src/upload/upload.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class UploadService {
-    private readonly uploadPath;
-    constructor();
-    private ensureUploadDir;
-    getFilePath(filename: string): string;
-    getFileUrl(filename: string): string;
-}
